585728c7c0f068b5618da34076bfe2801b97ea99
Elvis Li <elvis.li@refineddata.com>
2017-01-04 16:08:23 -0500

    RT-1564#CSS in certain blocks needs tweaking

--- block_refinedtools.php
+++ block_refinedtools.php
@@ -50,67 +50,79 @@ class block_refinedtools extends block_list {
         }
 
         if (has_capability('moodle/course:update', $context)) {
-            $this->content->items[] = '<a target="connect" href="' . $CFG->wwwroot . '/filter/connect/launch.php">' . get_string('launch', 'connectmeeting') . '</a>';
-            $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/mod/connectmeeting/pix/icon.png" class="icon" alt="Launch Adobe Connect" />';
+            $icon = '<img src="' . $CFG->wwwroot . '/mod/connectmeeting/pix/icon.png" class="icon" alt="Launch Adobe Connect" />';
+            $this->content->items[] = '<a target="connect" href="' . $CFG->wwwroot . '/filter/connect/launch.php">' . $icon . get_string('launch', 'connectmeeting') . '</a>';
+            $this->content->icons[] = '';
 
             if (!has_capability('moodle/course:view', $context)) { // Just return
                 return $this->content;
             }
 
             if ($DB->get_record('config_plugins', array('plugin' => 'local_reminders', 'name' => 'version'))) {
-                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/reminders/reminders.php">' . get_string('browse', 'local_reminders') . '</a>';
-                $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/email') . '" class="icon" alt="Browse Reminders" />';
+                $icon = '<img src="' . $OUTPUT->pix_url('i/email') . '" class="icon" alt="Browse Reminders" />';
+                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/reminders/reminders.php">' . $icon . get_string('browse', 'local_reminders') . '</a>';
+                $this->content->icons[] = '';
             }
 
             if ($DB->get_record('config_plugins', array('plugin' => 'local_managers', 'name' => 'version'))) {
-                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/managers/managers.php">' . get_string('pluginname', 'local_managers') . '</a>';
-                $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/group') . '" class="icon" alt="' . get_string('pluginname', 'local_managers') . ' />';
+                $icon = '<img src="' . $OUTPUT->pix_url('i/group') . '" class="icon" alt="' . get_string('pluginname', 'local_managers') . '" />';
+                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/managers/managers.php">' . $icon . get_string('pluginname', 'local_managers') . '</a>';
+                $this->content->icons[] = '';
             }
 
             if ($DB->get_record('config_plugins', array('plugin' => 'enrol_token', 'name' => 'version'))) {
-                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/token/browse.php">' . get_string('addedit', 'enrol_token') . '</a>';
-                $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/key') . '" class="icon" alt="Enrol Tokens" />';
+                $icon = '<img src="' . $OUTPUT->pix_url('i/key') . '" class="icon" alt="Enrol Tokens" />';
+                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/token/browse.php">' . $icon . get_string('addedit', 'enrol_token') . '</a>';
+                $this->content->icons[] = '';
             }
 
             if ($DB->get_record('config_plugins', array('plugin' => 'local_rolealerts', 'name' => 'version'))) {
-                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/rolealerts/index.php">' . get_string('pluginname', 'local_rolealerts') . '</a>';
-                $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/group') . '" class="icon" alt="' . get_string('pluginname', 'local_rolealerts') . ' />';
+                $icon = '<img src="' . $OUTPUT->pix_url('i/group') . '" class="icon" alt="' . get_string('pluginname', 'local_rolealerts') . '" />';
+                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/rolealerts/index.php">' . $icon . get_string('pluginname', 'local_rolealerts') . '</a>';
+                $this->content->icons[] = '';
             }
 
             if (isset($CFG->local_workbook) AND $CFG->local_workbook AND $course->id != SITEID) {
                 if ($DB->get_record('config_plugins', array('plugin' => 'local_webinars', 'name' => 'version')) AND $course->id != SITEID) {
-                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/webinars/workbook.php?id=' . $course->id . '">' . get_string('workbook', 'local_webinars') . '</a>';
-                    $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/stats') . '" class="icon" alt="' . get_string('workbook', 'local_webinars') . ' />';
+                    $icon = '<img src="' . $OUTPUT->pix_url('i/stats') . '" class="icon" alt="' . get_string('workbook', 'local_webinars') . '" />';
+                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/webinars/workbook.php?id=' . $course->id . '">' . $icon . get_string('workbook', 'local_webinars') . '</a>';
+                    $this->content->icons[] = '';
                 }
 
                 if ($DB->get_record('config_plugins', array('plugin' => 'local_webinars', 'name' => 'version')) AND $course->id != SITEID) {
-                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/webinars/dashboard.php?id=' . $course->id . '">' . get_string('dashboard', 'local_webinars') . '</a>';
-                    $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/stats') . '" class="icon" alt="' . get_string('dashboard', 'local_webinars') . ' />';
+                    $icon = '<img src="' . $OUTPUT->pix_url('i/stats') . '" class="icon" alt="' .  get_string('dashboard', 'local_webinars') . '" />';
+                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/local/webinars/dashboard.php?id=' . $course->id . '">' . $icon . get_string('dashboard', 'local_webinars') . '</a>';
+                    $this->content->icons[] = '';
                 }
             }
 
             if (isset($CFG->tsession_autotutor) AND $course->id != SITEID) {
-                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/mod/tsession/tutors.php?course=' . $course->id . '">' . get_string('tutors', 'tsession') . '</a>';
-                $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
+                $icon = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
+                $this->content->items[] = '<a href="' . $CFG->wwwroot . '/mod/tsession/tutors.php?course=' . $course->id . '">' . $icon . get_string('tutors', 'tsession') . '</a>';
+                $this->content->icons[] = '';
             }
 
             if ( $course->id != SITEID ){
                 $enrol = $DB->get_record('enrol', array('enrol' => 'rtform', 'courseid' => $course->id, 'status' => 0));
                 if (!empty($enrol)){
-                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/rtform/dashboard.php?id=' . $course->id . '">' . get_string('rtformdashboard', 'enrol_rtform') . '</a>';
-                    $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
-                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/rtform/workbook.php?id=' . $course->id . '">' . get_string('rtformworkbook', 'enrol_rtform') . '</a>';
-                    $this->content->icons[] = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
+                    $icon = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
+                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/rtform/dashboard.php?id=' . $course->id . '">' . $icon . get_string('rtformdashboard', 'enrol_rtform') . '</a>';
+                    $this->content->icons[] = '';
+                    $icon = '<img src="' . $OUTPUT->pix_url('i/users') . '" class="icon" alt="Tutors" />';
+                    $this->content->items[] = '<a href="' . $CFG->wwwroot . '/enrol/rtform/workbook.php?id=' . $course->id . '">' . $icon . get_string('rtformworkbook', 'enrol_rtform') . '</a>';
+                    $this->content->icons[] = '';
                 }
             }
 
-            $this->content->items[] = '<a target="_blank" href="http://support.refineddata.com">' . get_string('rt_support_center', 'block_refinedtools') . '</a>';
-            $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/blocks/refinedtools/pix/rds_16x16.png" class="icon" />';
+            $icon = '<img src="' . $CFG->wwwroot . '/blocks/refinedtools/pix/rds_16x16.png" class="icon" />';
+            $this->content->items[] = '<a target="_blank" href="http://support.refineddata.com">' . $icon . get_string('rt_support_center', 'block_refinedtools') . '</a>';
+            $this->content->icons[] = '';
         }
 
         if (has_capability('block/refinedtools:send_sso_meeting_link', $context)) {
-            $this->content->items[] = '<a href="'.$CFG->wwwroot.'/blocks/refinedtools/send_meeting_links.php">' . get_string('sso_block_link', 'block_refinedtools') . '</a>';
-            $this->content->icons[] = '<img src="' . $CFG->wwwroot . '/filter/connect/images/meeting_sm.jpg" height=16 width=16 class="icon" />';
+            $icon = '<img src="' . $CFG->wwwroot . '/filter/connect/images/meeting_sm.jpg" height=16 width=16 class="icon" />';
+            $this->content->items[] = '<a href="'.$CFG->wwwroot.'/blocks/refinedtools/send_meeting_links.php">' . $icon . get_string('sso_block_link', 'block_refinedtools') . '</a>';
+            $this->content->icons[] = '';
         }
 
         return $this->content;
